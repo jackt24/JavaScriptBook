21233a4dde5ef2e6f67508213ee4def9cb0ce253
jackt24 <48061691+jackt24@users.noreply.github.com>
2024-09-27 23:31:06 +0100

    added chapters 8 and 9

--- chapter8.js
+++ chapter8.js
@@ -10,36 +10,109 @@ console.log(encoded_new_uri)
 
 // 8.2 - Remove duplicates from an array
 
-arr = ["Laurence", "Mike", "Larry", "Kim","Joanne", "Laurence", "Mike", "Laurence", "Mike", "Laurence", "Mike"]
+// arr = ["Laurence", "Mike", "Larry", "Kim","Joanne", "Laurence", "Mike", "Laurence", "Mike", "Laurence", "Mike"]
 
-function checkDuplicates(array, index, value){
-    // console.log(index, value)
-    // console.log(value.indexOf(array))
-    if(value.indexOf(array)!=index){
-        return false
-    } else {
-        return true
-    }
-}
+// const arr2 = arr.filter((value, index, array) => {
+//     console.log(value, index, array.indexOf(value));
+//     return array.indexOf(value) === index;
+// });
 
-let funcVar = function() {
-    let filterArray = arr.filter(checkDuplicates)
-    return filterArray
-}
-
-console.log(funcVar())
+// console.log(arr2)
 
 // 8.3 - multiply all numbers in an array by 2
 
-arrNum = [1,2,3,4,5,6]
+// arrNum = [1,2,3,4,5,6]
 
-let mapped_arr = function(){
-    mappedArr = arrNum.map(x => 2*x)
-    console.log(mappedArr)
-}
+// const mapped_arr2 = arrNum.map(function(ele) {
+//     return ele*2;
+// });
 
-console.log(mapped_arr())
+// console.log(mapped_arr2)
 
-console.log(arrNum.map(x =>2*x))
+// console.log(arrNum.map(x =>2*x))
 
-// helo 
+// 8.4 Capitalise the first letter of each word and ensure rest are lower case
+
+// str = "thIs will be capiTalized for eAch word"
+// arr = []
+
+// function convertStuff(string){
+//     newStr = string.toLowerCase()
+//     arr = newStr.split(" ") 
+//     arr.forEach((ele, index) => {
+//         first = ele.slice(0,1).toUpperCase()
+//         second = ele.slice(1).toLowerCase()
+//         ele = first.concat(second)
+//         this.arr[index]= ele
+//     });
+//     newStr = arr.join(" ")
+//     console.log(newStr)
+// }
+
+// convertStuff(str)
+
+// 8.5 Lower case a string and replace all vowels
+
+// str = "I Love JavaScript"
+
+// str = str.toLowerCase()
+// console.log(str)
+
+// vowels = ["a", "e", "i", "o", "u"]
+
+// vowels.forEach((ele, index) =>{str = str.replaceAll(ele, index)});
+
+// console.log(str)
+
+// 8.6 Math equations 
+
+// console.log(Math.PI)
+
+// mCeil = Math.ceil(5.7)
+// mFloor = Math.floor(5.7)
+// console.log(mCeil, mFloor)
+
+// console.log(Math.random())
+
+// rand1 = Math.floor(Math.random()*11)
+// console.log(rand1)
+// rand2 = Math.ceil(Math.random()*10)
+// console.log(rand2)
+// rand3 = Math.floor(Math.random()*101)
+// console.log(rand3)
+
+// min = 1
+// max = 100
+
+// function minMax(min, max){
+//     val = Math.ceil(Math.random() * max)
+//     while (val<min){
+//         val = Math.ceil(Math.random() * max)
+//     }
+//     console.log(val)
+// }
+
+// for(i=0; i<100; i++){
+//     minMax(min, max)
+// }
+
+// 8.7
+
+let currentDate = new Date()
+console.log(currentDate)
+
+const months = [
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  let day = currentDate.getDate()
+  let year = currentDate.getFullYear()
+  let month = currentDate.getMonth()
+
+  date = months[currentDate.getMonth()]
+  console.log(date)
+
+  let myDate = `${date} ${day} ${year}`;
+
+  console.log(myDate)
---
+++ exercise8.js
@@ -0,0 +1,29 @@
+// str = "cats"
+// let scrambled = ""
+
+
+// function exercise(string){
+//     length = string.length
+//     // temp = ""
+//     for(i=0;i<length;i++){
+//         ran = string.charAt(Math.floor(Math.random()*string.length))
+//         console.log(ran)
+//         // temp += string[ran]
+//         // console.log(temp)
+//         // string = string.substr(0, ran) + string.substr(ran + 1)
+//         str = str.replace(ran,"")
+//     //    console.log(str)
+//         scrambled = scrambled + ran
+//     //    console.log(scrambled)
+//     }
+
+//     console.log(str, scrambled)
+// }
+
+// console.log(exercise(str))
+
+// Exercise 2
+
+date = new Date()
+enddate = Date.parse("December 12, 2025")
+console.log(enddate)
